2c366e5f245f953013c9e65e325ad37cc1c38009
MOzkutuk <mbaran1999@yahoo.com>
2023-10-25 12:57:53 +0200

    any commit

---
+++ src/main/java/com/oss/javabitbucket/JavaBitbucketApplication.java
@@ -0,0 +1,13 @@
+package com.oss.javabitbucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBitbucketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBitbucketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oss/javabitbucket/JavaBitbucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oss.javabitbucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBitbucketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
